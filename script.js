1a9f46af32a02182467023a013e341a925c7a2b4
bocman17 <hejlikt@gmail.com>
2021-08-02 13:35:03 +0200

    testing

---
+++ script.js
@@ -0,0 +1,15 @@
+let heading = document.querySelector("h1")
+
+// heading.addEventListener("mouseenter", function (e) {
+//     heading.style.color = "blue"
+// })
+
+// heading.addEventListener("mouseleave", function (e) {
+//     heading.style.color = "green"
+// })
+
+// heading.style.fontSize = "69px"
+
+// heading.style.display = "none"
+
+heading.style.fontWeight = 900
